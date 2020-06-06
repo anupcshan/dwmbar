c5206251dcc38c0d936fd493918f26760fea0e55
Anup Chenthamarakshan <anupcshan@gmail.com>
2020-06-05 21:50:51 -0700

    Print battery information every 5 sec

--- dwmbar.go
+++ dwmbar.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"strconv"
 	"strings"
 	"time"
 
@@ -21,6 +24,72 @@ func getTimeStr() <-chan string {
 	}()
 
 	return ch
+}
+
+func getBatteryStr() <-chan string {
+	ch := make(chan string)
+	go func() {
+		for {
+			ch <- batteryStr()
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	return ch
+}
+
+func formatDuration(d time.Duration) string {
+	hrs := d / time.Hour
+	d -= hrs * time.Hour
+	return fmt.Sprintf("%02d:%02d", hrs, d/time.Minute)
+}
+
+func batteryStr() string {
+	capacityBytes, err := ioutil.ReadFile("/sys/class/power_supply/BAT0/capacity")
+	if err != nil {
+		return err.Error()
+	}
+
+	chargeBytes, err := ioutil.ReadFile("/sys/class/power_supply/BAT0/charge_now")
+	if err != nil {
+		return err.Error()
+	}
+
+	chargeFullBytes, err := ioutil.ReadFile("/sys/class/power_supply/BAT0/charge_full")
+	if err != nil {
+		return err.Error()
+	}
+
+	currentBytes, err := ioutil.ReadFile("/sys/class/power_supply/BAT0/current_now")
+	if err != nil {
+		return err.Error()
+	}
+
+	statusBytes, err := ioutil.ReadFile("/sys/class/power_supply/BAT0/status")
+	if err != nil {
+		return err.Error()
+	}
+
+	capacity, _ := strconv.Atoi(strings.TrimSpace(string(capacityBytes)))
+	charge, _ := strconv.Atoi(strings.TrimSpace(string(chargeBytes)))
+	chargeFull, _ := strconv.Atoi(strings.TrimSpace(string(chargeFullBytes)))
+	current, _ := strconv.Atoi(strings.TrimSpace(string(currentBytes)))
+	status := strings.TrimSpace(string(statusBytes))
+
+	switch status {
+	case "Discharging":
+		timeRemaining := time.Duration((60*charge)/current) * time.Minute
+		return fmt.Sprintf("BAT: [D] %d%% (%s)", capacity, formatDuration(timeRemaining))
+	case "Charging":
+		if current == 0 {
+			return fmt.Sprintf("BAT: [F] %d%%", capacity)
+		}
+		timeRemaining := time.Duration((60*(chargeFull-charge))/current) * time.Minute
+		return fmt.Sprintf("BAT: [C] %d%% (%s)", capacity, formatDuration(timeRemaining))
+	case "Full":
+		return fmt.Sprintf("BAT: [F] %d%%", capacity)
+	}
+	return "BAT: Unknown"
 }
 
 type posStr struct {
@@ -59,6 +128,7 @@ func genStr(blocks []block) <-chan string {
 
 func main() {
 	blocks := []block{
+		getBatteryStr,
 		getTimeStr,
 	}
 
